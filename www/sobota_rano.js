e6fd82cfcbb08b895b237004eaf269b131ba33e2
Jacket44 <mateusz.trzeciak44@gmail.com>
2018-04-21 08:59:21 +0200

    pola

--- www/sobota_rano.js
+++ www/sobota_rano.js
@@ -5,7 +5,7 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1BhWtH6gLFCkWI2Z-35BdNZ9oAm8LcInuWrK3i_sspfs');
-    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, AG order by AG desc limit 25 label A "Nazwa Zastępu", B "Drużyna", C "Umundurowanie", D "Proporzec", E "Liczebność", F "Sprawność Turniejowa", G "Obozowisko", H "W dowód patriotyzmu", I "Z polecenia Geografa", J "Zielarz w opałach", K "W ukryciu przed wrogiem", L "W dowód patriotyzmu", M "W obliczu niebezpieczeństwa", N "Ranny Strzelec", O "Elminacja Transportu", P "Ukryty wśród drzew", Q "Jeden za wszysktich...", R "Sokole oko", S "By zdobyć szczyt ideałów", T "Rzeka historii", U "Jest 10 praw", V "Gra główna", AG "Suma punktow"');
+    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, AG order by AG desc limit 24 label A "Nazwa Zastępu", B "Drużyna", C "Umundurowanie", D "Proporzec", E "Liczebność", F "Sprawność Turniejowa", G "Obozowisko", H "W dowód patriotyzmu", I "Z polecenia Geografa", J "Zielarz w opałach", K "W ukryciu przed wrogiem", L "W dowód patriotyzmu", M "W obliczu niebezpieczeństwa", N "Ranny Strzelec", O "Elminacja Transportu", P "Ukryty wśród drzew", Q "Jeden za wszysktich...", R "Sokole oko", S "By zdobyć szczyt ideałów", T "Rzeka historii", U "Jest 10 praw", V "Gra główna", AG "Suma punktow"');
     query.send(handleQueryResponse);
 }
 
