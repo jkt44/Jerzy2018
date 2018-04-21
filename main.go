dacb034c4b2b92ded2541db00498aed9d1a632fd
Jacket44 <mateusz.trzeciak44@gmail.com>
2018-04-21 05:15:53 +0200

    m

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+func routine() {
+	for {
+		binary, lookErr := exec.LookPath("git")
+		if lookErr != nil {
+			panic(lookErr)
+		}
+		args := []string{"git", "push"}
+		env := os.Environ()
+		execErr := syscall.Exec(binary, args, env)
+		if execErr != nil {
+			panic(execErr)
+		}
+		time.Sleep(time.Duration(time.Minute))
+	}
+}
 
 func main() {
+	go routine()
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("www"))))
 	http.ListenAndServe(":8080", nil)
 }
